a0c79c84e161d324ea07cb625a71fc5dc4e17013
notorious9930 <damani.mccaskie@mycavehill.uwi.edu>
2019-03-12 16:57:43 -0400

    pointers.cpp

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void myswap(int * ptr1, int* ptr2)
 {
-  auto temp = ptr1;
-  ptr1 = ptr2;
-  ptr2 = temp;
+  auto temp = *ptr1; //they also have to be pointing to the variables here....
+  *ptr1 = *ptr2;
+  *ptr2 = temp;
 }
 
 
@@ -21,7 +21,7 @@ int main()
 //swap(a,b); //why does theirs work????   
  
   myswap(&a, &b); // but mine doesn't ?!?!?!?!!!???
-
+  //must use the reference to the variables in the calling function
   cout<<"a = "<<a<<", b = "<<b<<endl;
 
 
